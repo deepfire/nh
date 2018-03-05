ade49ef04dbda3e3095a3e8e109b0dc7ce0fefe3
Kosyrev Serge <serge.kosyrev@iohk.io>
2018-03-05 03:18:01 +0300

    Misc:  .dir-locals.el, gitignore update, shell.nix

---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ nixpkgs     ? import <nixpkgs> {}
+, pkgs        ? nixpkgs.pkgs, haskell ? pkgs.haskell
+, compiler    ? "ghc841"
+, ghcOrig     ? pkgs.haskell.packages."${compiler}"
+, tools       ? false
+, intero      ? tools
+}:
+let
+
+  ghc     = import ./packages.nix { inherit nixpkgs pkgs haskell compiler ghcOrig; };
+  default = import ./.;
+  drv     = ghc.callPackage default {};
+  drv'    = haskell.lib.overrideCabal
+            drv
+            (old: {
+              libraryHaskellDepends =
+                [ pkgs.cabal-install ];
+             });
+in
+  drv'.env
