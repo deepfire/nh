3266dfb434178e33104c07e66bc84812ed190f1f
Kosyrev Serge <serge.kosyrev@iohk.io>
2018-03-28 03:37:32 +0300

    cabal:  more deps

--- default.nix
+++ default.nix
@@ -1,8 +1,10 @@
 { mkDerivation, aeson, ansi-wl-pprint, base, base-unicode-symbols
-, Cabal, cabal2nix, containers, data-default-class, directory
-, distribution-nixpkgs, filepath, hnix, hourglass, language-nix
-, lens, optparse-applicative, parsers, pretty, req, semigroups
-, stdenv, temporary, text, text-format, trifecta
+, Cabal, cabal2nix, containers, control-bool, data-default-class
+, data-fix, directory, distribution-nixpkgs, filepath, generics-sop
+, github, hashable, hnix, hourglass, language-nix, lens
+, optparse-applicative, parsers, pretty, protolude, req, semigroups
+, stdenv, temporary, text, text-format, trifecta, turtle
+, unordered-containers
 }:
 mkDerivation {
   pname = "nh";
@@ -12,15 +14,18 @@ mkDerivation {
   isExecutable = true;
   libraryHaskellDepends = [
     aeson ansi-wl-pprint base base-unicode-symbols Cabal cabal2nix
-    containers data-default-class directory distribution-nixpkgs
-    filepath hnix hourglass language-nix lens optparse-applicative
-    parsers pretty req semigroups temporary text text-format trifecta
+    containers control-bool data-default-class data-fix directory
+    distribution-nixpkgs filepath generics-sop github hashable hnix
+    hourglass language-nix lens optparse-applicative parsers pretty
+    protolude req semigroups temporary text text-format trifecta turtle
+    unordered-containers
   ];
   executableHaskellDepends = [
     aeson ansi-wl-pprint base base-unicode-symbols Cabal cabal2nix
-    containers data-default-class distribution-nixpkgs hnix hourglass
-    language-nix lens optparse-applicative pretty req temporary text
-    text-format
+    containers control-bool data-default-class distribution-nixpkgs
+    generics-sop github hashable hnix hourglass language-nix lens
+    optparse-applicative pretty protolude req temporary text
+    text-format turtle unordered-containers
   ];
   description = "Nix/Haskell tooling";
   license = stdenv.lib.licenses.agpl3;
